dfef3fcaa12996e7de23b34743f804bf411b7228
Student <Student@A5-PC>
2017-07-13 16:52:40 +0430

    Add project files.

---
+++ Calculator.Service/CalculatorFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Service
+{
+    public static class CalculatorFactory
+    {
+        public ICalculator Get()
+        {
+
+        }
+
+    }
+
+    public enum Operation
+    {
+        sum = 1 ,
+        subtract = 2,
+
+    }
+}
---
+++ Calculator.Service/ICalculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Service
+{
+    public  interface ICalculator
+    {
+        decimal Calculate(decimal number1, decimal number2);
+    }
+}
