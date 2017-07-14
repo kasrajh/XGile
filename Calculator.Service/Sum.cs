5d8fccea460ce44f054f359ccdd6a0b4a7953d99
Student <Student@A5-PC>
2017-07-14 10:17:25 +0430

    new changes

---
+++ Calculator.Service/Sum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Service
+{
+    public class Sum : ICalculator
+    {
+        public decimal Calculate(decimal number1, decimal number2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
