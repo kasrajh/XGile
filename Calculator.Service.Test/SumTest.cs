d72bd65c33f26fa748760ef5a64782783e7000c3
ehsanpourtorab <ehsan.poorotrab@gmail.com>
2017-07-14 12:48:53 +0430

    Sum and tests

--- Calculator.Service.Test/SumTest.cs
+++ Calculator.Service.Test/SumTest.cs
@@ -13,5 +13,61 @@ namespace Calculator.Service.Test
             decimal result = sum.Calculate(1, 2);
             Assert.AreEqual(3, result);
         }
+
+        [TestMethod]
+        public void TestAddEmptyString()
+        {
+            var sum = new Sum();
+            int result = sum.Add("");
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestAddOneParameter()
+        {
+            var sum = new Sum();
+            int result = sum.Add("1");
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestAdd2Parameters()
+        {
+            var sum = new Sum();
+            int result = sum.Add("1,2");
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void TestAddUnknownParameters()
+        {
+            var sum = new Sum();
+            
+            int result = sum.Add("1,2,3,4,5,6,7");
+            Assert.AreEqual(28, result);
+        }
+
+        [TestMethod]
+        public void TestAddNewLineSeparator()
+        {
+            var sum = new Sum();
+
+            int result = sum.Add("1,2,3\n4,5\n6,7");
+            Assert.AreEqual(28, result);
+        }
+
+
+        [TestMethod]
+        public void TestAddSpecifySeprator()
+        {
+            var sum = new Sum();
+
+            int result = sum.Add("//;\n1;2");
+            Assert.AreEqual(3, result);
+        }
+
+
+
+
     }
 }
--- Calculator.Service/Sum.cs
+++ Calculator.Service/Sum.cs
@@ -12,5 +12,27 @@ namespace Calculator.Service
         {
             return number1 + number2;
         }
+
+        public int Add(string parameters)
+        {
+            if (string.IsNullOrEmpty(parameters))
+                return 0;
+
+            char[] separators = { ',', '\n' };
+            if (parameters.StartsWith("//"))
+            {
+                separators[2] = parameters[2];
+            }
+
+            string[] param = parameters.Split(separators);
+            int sum = 0;
+            foreach (var p in param)
+            {
+                sum += int.Parse(p);
+            }
+            return sum;
+        }
+
+       
     }
 }
