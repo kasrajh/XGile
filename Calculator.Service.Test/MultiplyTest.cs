9a4003ddbf4a02c54cb3569d9baa672b3419acb6
XGIlee-Group1 <Student@A6-PC>
2017-07-14 11:43:11 +0430

    Implemnting Multiply Class

---
+++ Calculator.Service.Test/MultiplyTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Service.Test
+{
+    /// <summary>
+    /// Summary description for UnitTest2
+    /// </summary>
+    [TestClass]
+    public class MultiplyTest
+    {
+        public MultiplyTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+
+            multiplier = new Multiply();
+        }
+
+        private Calculator.Service.Multiply multiplier;
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+
+        [TestMethod]
+        public void MultiplyTwoNumber()
+        {
+            //Arrange
+            var number1 = 5;
+            var number2 = 6;
+
+            //Act
+            var result = multiplier.Calculate(number1, number2);
+
+            //Assert
+            Assert.AreEqual(30, result);
+        }
+
+        [TestMethod]
+        public void ReturnZeroForLargeNamber()
+        {
+            //arrange
+            var number1 = 1000000000000;
+            var number2 = 200000000000;
+
+            //act
+            var result = multiplier.Calculate(number1,number2);
+
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void returnexceptionforzero()
+        {
+            //arrange 
+            var number1 = 0;
+            var number2 = 589;
+
+            //act
+            var result = multiplier.Calculate(number1, number2);
+
+        }
+            //a
+    }
+}
+
---
+++ Calculator.Service/Multiply.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Service
+{
+    public class Multiply : ICalculator
+    {
+        public decimal Calculate(decimal number1, decimal number2)
+        {
+            CheckArgumant(number1, number2);
+
+            return CalculateMultiple(number1, number2);
+        }
+
+        private decimal CalculateMultiple(decimal number1, decimal number2)
+        {
+            var upperBoundry = 10000;
+            if (number1 > upperBoundry || number2 > upperBoundry)
+                return 0;
+            else
+                return number1* number2;
+        }
+
+        private static void CheckArgumant(decimal number1, decimal number2)
+        {
+            if (number1 == 0 || number2 == 0)
+                throw new ArgumentException("ورودی صحیح نیست");
+        }
+    }
+}
